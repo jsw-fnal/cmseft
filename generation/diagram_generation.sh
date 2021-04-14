9d9b5527c283aec90d9e79f416175236431299c4
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2021-04-14 18:13:30 +0200

    Porting code to nanoAODv7 CMSSW

--- generation/diagram_generation.sh
+++ generation/diagram_generation.sh
@@ -42,7 +42,7 @@ if [ -z ${name} ]; then
 fi
 
 MG_EXT=".tar.gz"
-MG=MG5_aMC_v2.6.5$MG_EXT
+MG=MG5_aMC_v2.9.18$MG_EXT
 MGSOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/$MG
 MGBASEDIRORIG=$(echo ${MG%$MG_EXT} | tr "." "_")
 wget --no-check-certificate ${MGSOURCE}
@@ -55,7 +55,7 @@ cd $WORKDIR
 cp -rp ${PRODHOME}/addons/models/* ${MGBASEDIRORIG}/models/
 
 if [ -e $CARDSDIR/${name}_restrict_custom.dat ]; then
-  cp $CARDSDIR/${name}_restrict_custom.dat ./Cards/restrict_custom.dat
+  cp $CARDSDIR/${name}_restrict_custom.dat restrict_custom.dat
   for MDDIR in ${MGBASEDIRORIG}/models/*/
   do
       echo $CARDSDIR/${name}_restrict_custom.dat $MDDIR/restrict_custom.dat 
@@ -65,11 +65,60 @@ fi
 
 cp $CARDSDIR/${name}_proc_card.dat ${name}_proc_card.dat
 echo "display diagrams ./" >> ${name}_proc_card.dat
+echo "display diagrams_text ./" >> ${name}_proc_card.dat
+
+pushd $MGBASEDIRORIG
+
+cat $PRODHOME/patches/*.patch | patch -p1
+cp -r $PRODHOME/PLUGIN/CMS_CLUSTER/ PLUGIN/ 
+
+echo "set auto_update 0" > mgconfigscript
+echo "set automatic_html_opening False" >> mgconfigscript
+echo "set auto_convert_model True" >> mgconfigscript
+echo "set run_mode 2" >> mgconfigscript
+echo "save options --all" >> mgconfigscript
+
+./bin/mg5_aMC mgconfigscript
+
+#load extra models if needed
+if [ -e $CARDSDIR/${name}_extramodels.dat ]; then
+	echo "Loading extra models specified in $CARDSDIR/${name}_extramodels.dat"
+	#strip comments
+	sed 's:#.*$::g' $CARDSDIR/${name}_extramodels.dat | while read -r model || [ -n "$model" ]
+do
+	#get needed BSM model
+	if [[ $model = *[!\ ]* ]]; then
+		echo "Loading extra model $model"
+		wget --no-check-certificate https://cms-project-generators.web.cern.ch/cms-project-generators/$model	
+		pushd models
+		if [[ $model == *".zip"* ]]; then
+			unzip ../$model
+		elif [[ $model == *".tgz"* ]]; then
+			tar zxvf ../$model
+		elif [[ $model == *".tar"* ]]; then
+			tar xavf ../$model
+		else 
+			echo "A BSM model is specified but it is not in a standard archive (.zip or .tar)"
+		fi
+		popd
+	fi
+done
+fi
+
+ls
+pwd
+
+if ls $CARDSDIR/${name}*.patch; then
+	echo "    WARNING: Applying custom user patch. I hope you know what you're doing!"
+	cat $CARDSDIR/${name}*.patch | patch -p 1 -f
+fi
+
+popd
 
 ${MGBASEDIRORIG}/bin/mg5_aMC ${name}_proc_card.dat
 
 # remove all the zero coefficients
-sed -e "s/NP\w*=0, //g" -e "s/, SMHLOOP=0//" -e "s/NP=1, //" -i *.eps
+#sed -e "s/NP\w*=0, //g" -e "s/, SMHLOOP=0//" -e "s/NP=1, //" -i *.eps
 
 PDFOUT="../${name}_diagrams"
 mkdir -p $PDFOUT
@@ -79,4 +128,4 @@ do
 done
 
 cd $PRODHOME/
-rm -rf $WORKDIR
+#rm -rf $WORKDIR
--- generation/fragments/pythia_fragment.py
+++ generation/fragments/pythia_fragment.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/eos/uscms/store/user/dspitzba/TT01j_tutorial_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
+    args = cms.vstring('/bar/TT01j_tutorial_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
