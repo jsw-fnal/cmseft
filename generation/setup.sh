1d9106ed4f7aebe6b9df235fb30e6cd6e9996b43
Jon Wilson <jsw@fnal.gov>
2025-02-20 15:15:24 -0600

    Make setup.sh get the madjax EFT nanoAOD code

--- generation/setup.sh
+++ generation/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d genproductions ]; then
-  git clone https://github.com/danbarto/genproductions -b fix_patching --depth 1
+  git clone https://github.com/jsw-fnal/genproductions.git -b feature/madjax --depth 1
 fi
 
 # in case this is not already done, setup cms packaging commands
@@ -16,14 +16,8 @@ if [ ! -d CMSSW_13_0_14 ]; then
 
   git cms-addpkg PhysicsTools/NanoAOD
   cd PhysicsTools/NanoAOD/
-  git remote add eftfit https://github.com/danbarto/cmssw.git
-  git fetch eftfit
-
-  # these might not work yet
-  git cherry-pick ffba2640cf7d8da6b8b36559c64e90a2076153e9
-  git cherry-pick 416096620ec9d477caad74704c40d3d7d695e06a
-  git cherry-pick f9a76b864ebf40833f73b03778634b553aecfc98
-  git cherry-pick 48581ffb94b1957203160428745b69048a7ffc94
+  git remote add eftfit https://github.com/jsw-fnal/cmssw.git
+  git checkout eftfit/feature/MadJaxEFTweight
 
   cd ../../
   git clone https://github.com/danbarto/EFTGenReader.git
